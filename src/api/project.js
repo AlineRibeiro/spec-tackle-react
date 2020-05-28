8f524b1f690484dbb0bb9ae8eb4e88d4572383b7
Aline Ribeiro <alinear.mail@gmail.com>
2020-05-28 14:43:28 -0700

    Create project post request

--- src/api/project.js
+++ src/api/project.js
@@ -4,6 +4,19 @@ export const Project = {
   index() {
     return fetch(`${baseUrl}/projects`, {
     }).then(res => res.json());
+  },
+  create(params) {
+    return fetch(`${baseUrl}/projects`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(params)
+    }).then(res => res.json());
   }
 };
+
+
+
+
 
