590be1e65cdd7e90b081c5c6115bd79ef3778f8a
Aline Ribeiro <alinear.mail@gmail.com>
2020-05-29 15:38:35 -0700

    Code cleanup

--- src/api/project.js
+++ src/api/project.js
@@ -2,31 +2,23 @@ import { baseUrl } from "../config";
 
 export const Project = {
   index() {
-    return fetch(`${baseUrl}/projects`, {
-    }).then(res => res.json());
+    return fetch(`${baseUrl}/projects`, {}).then((res) => res.json());
   },
   create(params) {
     return fetch(`${baseUrl}/projects`, {
       method: "POST",
       headers: {
-        "Content-Type": "application/json"
+        "Content-Type": "application/json",
       },
-      body: JSON.stringify(params)
-    })
-      .then(res => res.json());
+      body: JSON.stringify(params),
+    }).then((res) => res.json());
   },
   show(id) {
-    return fetch(`${baseUrl}/projects/${id}`, {
-    }).then(res => res.json());
+    return fetch(`${baseUrl}/projects/${id}`, {}).then((res) => res.json());
   },
   destroy(id) {
     return fetch(`${baseUrl}/projects/${id}`, {
-      method: "DELETE"
-    }).then(res => res.json());
-  }
+      method: "DELETE",
+    }).then((res) => res.json());
+  },
 };
-
-
-
-
-
--- src/pages/ProjectsIndexPage.js
+++ src/pages/ProjectsIndexPage.js
@@ -2,29 +2,26 @@ import React, { useState, useEffect } from "react";
 import { Project } from "../api/project";
 import { Link } from "react-router-dom";
 
-
 export const ProjectIndexPage = () => {
   let [projectIndex, setProjectIndex] = useState({
-    projects: []
+    projects: [],
   });
 
   useEffect(() => {
-    Project.index().then(response => {
-      setProjectIndex({ projects: response.projects});
-    })
+    Project.index().then((response) => {
+      setProjectIndex({ projects: response.projects });
+    });
   }, []);
   return (
     <div>
       <h1>My projects</h1>
       <ul>
-        {projectIndex.projects.map(project => (
-          <li key={project.id} >
-            <Link to={`/projects/${project.id}`}>
-              {project.name}
-            </Link>
+        {projectIndex.projects.map((project) => (
+          <li key={project.id}>
+            <Link to={`/projects/${project.id}`}>{project.name}</Link>
           </li>
         ))}
       </ul>
     </div>
-  )
+  );
 };
