755d388e564966e0580384ba73d48bff315fdfb4
Aline Ribeiro <alinear.mail@gmail.com>
2020-06-01 16:42:24 -0700

    Link logo to home page

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,15 +2,16 @@ import React from "react";
 import { Menu } from 'semantic-ui-react'
 
 export const  NavBar = (props) => {
-
     return (
       <Menu secondary>
+        <a href="/">
           <img
             src='../../images/spec-tackle_logo.png'
             alt='logo'
             width="200px"
             height="auto"
           />
+        </a>
         <Menu.Item
           name='howDoesItWork'
         >
