a68bb08d9d8930c1a4bb1b9a8342d64944fc8e7a
Aline Ribeiro <alinear.mail@gmail.com>
2020-06-02 13:30:10 -0700

    Add grid to home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,22 +1,34 @@
 import React from "react";
-import { Container } from "semantic-ui-react";
+import { Container, Grid } from "semantic-ui-react";
 
 export const Home = (props) => {
   return (
     <Container className="home">
-      <h1>Automated Software Testing with Cypress for your business</h1>
-
-      <p>
-        <span className="check-sign">&#10003;</span> Say goodbye to QA! Automate
-        your tests
-      </p>
-
-      <p>
-        <span className="check-sign">&#10003;</span> Savings: you will save 68%
-        during your app life{" "}
-      </p>
-
-      <button className="start-button">I want to test my app now</button>
+      <Grid columns="equal">
+        <Grid.Column width={10}>
+          <h1>Automated Software Testing with Cypress for your business</h1>
+          <br />
+          <p>
+            We are a team of software development consultants that know
+            everything about <br /> testing, so that you dont have to worry
+            about it.
+          </p>
+          <br />
+          <p>
+            <span className="check-sign">&#10003;</span> Say goodbye to QA.
+            Automate your tests!
+          </p>
+          <br />
+          <p>
+            <span className="check-sign">&#10003;</span> Savings: you will save
+            68% during your app life.
+          </p>
+          <br />
+          <br />
+          <button className="start-button">I want to test my app now</button>
+        </Grid.Column>
+        <Grid.Column width={6}>2</Grid.Column>
+      </Grid>
     </Container>
   );
 };
