e22e3291142642d5af3168d620cdffb0729ae888
Aline Ribeiro <alinear.mail@gmail.com>
2020-06-02 14:27:56 -0700

    Add rows to grid and refactor navbar styles

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,29 +5,39 @@ export const Home = (props) => {
   return (
     <Container className="home">
       <Grid columns="equal">
-        <Grid.Column width={10}>
-          <h1>Automated Software Testing with Cypress for your business</h1>
-          <br />
-          <p>
-            We are a team of software development consultants that know
-            everything about <br /> testing, so that you dont have to worry
-            about it.
-          </p>
-          <br />
-          <p>
-            <span className="check-sign">&#10003;</span> Say goodbye to QA.
-            Automate your tests!
-          </p>
-          <br />
-          <p>
-            <span className="check-sign">&#10003;</span> Savings: you will save
-            68% during your app life.
-          </p>
-          <br />
-          <br />
-          <button className="start-button">I want to test my app now</button>
-        </Grid.Column>
-        <Grid.Column width={6}>2</Grid.Column>
+        <Grid.Row>
+          <Grid.Column width={9}>
+            <h1>
+              Automated Software Testing with <br /> Cypress for your business
+            </h1>
+            <br />
+            <p>
+              We are a team of software development consultants that know
+              <br /> everything about testing, so that you dont have to worry
+              <br /> about it.
+            </p>
+            <br />
+            <p>
+              <span className="check-sign">&#10003;</span> Say goodbye to QA.
+              Automate your tests!
+            </p>
+            <br />
+            <p>
+              <span className="check-sign">&#10003;</span> Savings: you will
+              save 68% during your app life.
+            </p>
+            <br />
+            <br />
+            <button className="start-button">I want to test my app now</button>
+          </Grid.Column>
+          <Grid.Column width={7}>
+            <img
+              className="side-design"
+              src="../../images/sideDesign.png"
+              alt="blue design"
+            />
+          </Grid.Column>
+        </Grid.Row>
       </Grid>
     </Container>
   );
